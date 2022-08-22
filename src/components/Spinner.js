817649a6d0c8f8fc1a8ea50096ea8cb8eeb5896e
Josip <111125255+josip0920@users.noreply.github.com>
2022-08-22 09:57:28 +0300

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Spinner = () => {
-    return(
-        <div>
-            Spinner
-        </div>
+    return (
+        <Stack sx={{ background: '#2e79ed', height: '100%' }} justifyContent='center' alignItems='center'>
+            <CircularProgress color="inherit" />
+        </Stack>
     )
 }
 
