be4d1bd59f0c1209933f29ccb295de076dbe8a01
Josip0920 <111125255+josip0920@users.noreply.github.com>
2022-09-27 08:33:19 +0300

    Update AdminSetting.js

--- src/pages/AdminSetting.js
+++ src/pages/AdminSetting.js
@@ -5,155 +5,217 @@ import Typography from "@mui/material/Typography";
 import { SETTINGS } from "../config";
 
 const AdminSetting = () => {
-  const [drum, setDrum] = useState()
-  const [guitar, setGuitar] = useState()
-  const [synth, setSynth] = useState()
+    const [drum, setDrum] = useState();
+    const [guitar, setGuitar] = useState();
+    const [synth, setSynth] = useState();
 
-  return (
-    <Stack sx={{ height: '84%', width: 480 }}>
-      <Stack sx={{ height: '100%', overflow: 'auto' }} className="home-setting">
-        <Stack
-          justifyContent='center'
-          sx={{
-            py: '60%'
-          }}
-        >
-          <Stack alignItems='center' sx={{ pt: 10 }}>
-            <Typography fontSize={64} fontFamily={'Michroma'} color='white'>
-              ARTIST
-            </Typography>
-            <Typography fontSize={56} fontFamily={'Michroma'} color='#858ac7'>
-              SONGTITLE
-            </Typography>
-          </Stack>
+    return (
+        <Stack sx={{ width: 500 }}>
+            <Stack
+                sx={{ height: "100%", overflow: "auto" }}
+                className="home-setting"
+                spacing={1}
+            >
+                <Typography
+                    letterSpacing={0.05}
+                    fontSize={14}
+                    fontWeight={900}
+                    color="#000000"
+                    fontFamily={"Inter"}
+                >
+                    DEFAULT SETTINGS
+                </Typography>
+                <Stack
+                    sx={{
+                        pb: "20%",
+                    }}
+                    spacing={10}
+                >
+                    <Stack spacing={3}>
+                        <Typography
+                            letterSpacing={0.05}
+                            fontSize={14}
+                            fontWeight={900}
+                            color="rgba(46, 46, 46, 0.75)"
+                            fontFamily={"Inter"}
+                        >
+                            DRUM PATTERN
+                        </Typography>
+                        <Stack direction="row" justifyContent={"space-between"}>
+                            {SETTINGS.drum.map((item, index) => {
+                                return (
+                                    <Stack
+                                        key={index}
+                                        spacing={2}
+                                        alignItems="center"
+                                        sx={{
+                                            width: "20%",
+                                        }}
+                                    >
+                                        <Button
+                                            onClick={() => setDrum(item.id)}
+                                            sx={{
+                                                background:
+                                                    item.id == drum
+                                                        ? "#1D1B8C"
+                                                        : "transparent",
+                                                width: 47,
+                                                height: 47,
+                                                minWidth: 0,
+                                                borderRadius: 47,
+                                                border: "1px solid rgba(46, 46, 46, 0.5)",
+                                                color: "white",
+                                                "&:hover": {
+                                                    background: "#1d1b8ce3",
+                                                },
+                                                "&:active": {
+                                                    background: "#1D1B8C",
+                                                },
+                                            }}
+                                        ></Button>
+                                        <Typography
+                                            textAlign="center"
+                                            color={
+                                                item.id == drum
+                                                    ? "#1D1B8C"
+                                                    : "rgba(46, 46, 46, 0.5)"
+                                            }
+                                            fontSize={12}
+                                            fontWeight={700}
+                                            fontFamily={"Inter"}
+                                        >
+                                            {item.title}
+                                        </Typography>
+                                    </Stack>
+                                );
+                            })}
+                        </Stack>
+                    </Stack>
+                    <Stack spacing={3}>
+                        <Typography
+                            letterSpacing={0.05}
+                            fontSize={14}
+                            fontWeight={900}
+                            color="rgba(46, 46, 46, 0.75)"
+                            fontFamily={"Inter"}
+                            sx={{ ml: 2 }}
+                        >
+                            GUITAR LEAD
+                        </Typography>
+                        <Stack direction="row" justifyContent={"space-between"}>
+                            {SETTINGS.guitar.map((item, index) => {
+                                return (
+                                    <Stack
+                                        key={index}
+                                        spacing={2}
+                                        alignItems="center"
+                                        sx={{
+                                            width: "20%",
+                                        }}
+                                    >
+                                        <Button
+                                            onClick={() => setGuitar(item.id)}
+                                            sx={{
+                                                background:
+                                                    item.id == guitar
+                                                        ? "#1D1B8C"
+                                                        : "transparent",
+                                                width: 47,
+                                                height: 47,
+                                                minWidth: 0,
+                                                borderRadius: 47,
+                                                border: "1px solid rgba(46, 46, 46, 0.5)",
+                                                color: "white",
+                                                "&:hover": {
+                                                    background: "#1d1b8ce3",
+                                                },
+                                                "&:active": {
+                                                    background: "#1D1B8C",
+                                                },
+                                            }}
+                                        ></Button>
+                                        <Typography
+                                            textAlign="center"
+                                            color={
+                                                item.id == guitar
+                                                    ? "#1D1B8C"
+                                                    : "rgba(46, 46, 46, 0.5)"
+                                            }
+                                            fontSize={12}
+                                            fontWeight={700}
+                                            fontFamily={"Inter"}
+                                        >
+                                            {item.title}
+                                        </Typography>
+                                    </Stack>
+                                );
+                            })}
+                        </Stack>
+                    </Stack>
+                    <Stack spacing={3}>
+                        <Typography
+                            letterSpacing={0.05}
+                            fontSize={14}
+                            fontWeight={900}
+                            color="rgba(46, 46, 46, 0.75)"
+                            fontFamily={"Inter"}
+                            sx={{ ml: 2 }}
+                        >
+                            SYNTH STYLE
+                        </Typography>
+                        <Stack direction="row" justifyContent={"space-between"}>
+                            {SETTINGS.synth.map((item, index) => {
+                                return (
+                                    <Stack
+                                        key={index}
+                                        spacing={2}
+                                        alignItems="center"
+                                        sx={{
+                                            width: "20%",
+                                        }}
+                                    >
+                                        <Button
+                                            onClick={() => setSynth(item.id)}
+                                            sx={{
+                                                background:
+                                                    item.id == synth
+                                                        ? "#1D1B8C"
+                                                        : "transparent",
+                                                width: 47,
+                                                height: 47,
+                                                minWidth: 0,
+                                                borderRadius: 47,
+                                                border: "1px solid rgba(46, 46, 46, 0.5)",
+                                                color: "white",
+                                                "&:hover": {
+                                                    background: "#1d1b8ce3",
+                                                },
+                                                "&:active": {
+                                                    background: "#1D1B8C",
+                                                },
+                                            }}
+                                        ></Button>
+                                        <Typography
+                                            color={
+                                                item.id == synth
+                                                    ? "#1D1B8C"
+                                                    : "rgba(46, 46, 46, 0.5)"
+                                            }
+                                            fontSize={12}
+                                            fontWeight={700}
+                                            fontFamily={"Inter"}
+                                        >
+                                            {item.title}
+                                        </Typography>
+                                    </Stack>
+                                );
+                            })}
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </Stack>
         </Stack>
-        <Stack
-          sx={{
-            pb: '20%',
-          }}
-          spacing={10}
-        >
-          <Stack spacing={3}>
-            <Typography letterSpacing={0.05} fontSize={14} fontWeight={900} color="white" fontFamily={'Inter'} sx={{ ml: 2 }}>DRUM PATTERN</Typography>
-            <Stack direction='row' justifyContent={'space-between'}>
-              {
-                SETTINGS.drum.map((item, index) => {
-                  return (
-                    <Stack
-                      key={index}
-                      spacing={2}
-                      alignItems='center'
-                      sx={{
-                        width: '20%',
-                      }}
-                    >
-                      <Button
-                        onClick={() => setDrum(item.id)}
-                        sx={{
-                          background: item.id == drum ? "white" : 'transparent',
-                          width: 47,
-                          height: 47,
-                          minWidth: 0,
-                          borderRadius: 47,
-                          border: '1px solid white',
-                          color: 'white',
-                          '&:hover': {
-                            background: '#ffffffe0'
-                          },
-                          '&:active': {
-                            background: 'white'
-                          }
-                        }}
-                      ></Button>
-                      <Typography textAlign='center' color={'rgba(255, 255, 255, 0.5)'} fontSize={12} fontWeight={700} fontFamily={'Inter'}>{item.title}</Typography>
-                    </Stack>
-                  )
-                })
-              }
-            </Stack>
-          </Stack>
-          <Stack spacing={3}>
-            <Typography letterSpacing={0.05} fontSize={14} fontWeight={900} color="white" fontFamily={'Inter'} sx={{ ml: 2 }}>GUITAR LEAD</Typography>
-            <Stack direction='row' justifyContent={'space-between'}>
-              {
-                SETTINGS.guitar.map((item, index) => {
-                  return (
-                    <Stack
-                      key={index}
-                      spacing={2}
-                      alignItems='center'
-                      sx={{
-                        width: '20%'
-                      }}
-                    >
-                      <Button
-                        onClick={() => setGuitar(item.id)}
-                        sx={{
-                          background: item.id == guitar ? "white" : 'transparent',
-                          width: 47,
-                          height: 47,
-                          minWidth: 0,
-                          borderRadius: 47,
-                          border: '1px solid white',
-                          color: 'white',
-                          '&:hover': {
-                            background: '#ffffffe0'
-                          },
-                          '&:active': {
-                            background: 'white'
-                          }
-                        }}
-                      ></Button>
-                      <Typography textAlign='center' color={'rgba(255, 255, 255, 0.5)'} fontSize={12} fontWeight={700} fontFamily={'Inter'}>{item.title}</Typography>
-                    </Stack>
-                  )
-                })
-              }
-            </Stack>
-          </Stack>
-          <Stack spacing={3}>
-            <Typography letterSpacing={0.05} fontSize={14} fontWeight={900} color="white" fontFamily={'Inter'} sx={{ ml: 2 }}>SYNTH STYLE</Typography>
-            <Stack direction='row' justifyContent={'space-between'}>
-              {
-                SETTINGS.synth.map((item, index) => {
-                  return (
-                    <Stack
-                      key={index}
-                      spacing={2}
-                      alignItems='center'
-                      sx={{
-                        width: '20%'
-                      }}
-                    >
-                      <Button
-                        onClick={() => setSynth(item.id)}
-                        sx={{
-                          background: item.id == synth ? "white" : 'transparent',
-                          width: 47,
-                          height: 47,
-                          minWidth: 0,
-                          borderRadius: 47,
-                          border: '1px solid white',
-                          color: 'white',
-                          '&:hover': {
-                            background: '#ffffffe0'
-                          },
-                          '&:active': {
-                            background: 'white'
-                          }
-                        }}
-                      ></Button>
-                      <Typography color={'rgba(255, 255, 255, 0.5)'} fontSize={12} fontWeight={700} fontFamily={'Inter'}>{item.title}</Typography>
-                    </Stack>
-                  )
-                })
-              }
-            </Stack>
-          </Stack>
-        </Stack>
-      </Stack>
-    </Stack>
-  )
-}
+    );
+};
 
 export default AdminSetting;
