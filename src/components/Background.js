c4ae7f14706d120c1b4ae045cde0a8435ce3fda1
monte0920 <eugene20920@gmail.com>
2022-10-18 17:52:53 +0300

    Update Background.js

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
     DoubleSide,
@@ -11,39 +11,55 @@ import {
 
 const twoPi = Math.PI * 2;
 
-const data = {
-    radius: 15,
-    widthSegments: 32,
-    heightSegments: 16,
-    phiStart: 0,
-    phiLength: twoPi,
-    thetaStart: 0,
-    thetaLength: Math.PI,
-};
-new SphereGeometry(
-    data.radius,
-    data.widthSegments,
-    data.heightSegments,
-    data.phiStart,
-    data.phiLength,
-    data.thetaStart,
-    data.thetaLength
-);
-
 const Detect = () => {
     const { camera } = useThree();
     camera.position.z = 30;
 };
 
 const MyGroup = (props) => {
-    const { playing } = props;
+    const [widthSegments, setwidthSegments] = useState(32);
+    const { playing, playingTracks, currentTime, duration } = props;
+
+    const data = {
+        radius: 15,
+        widthSegments: 32,
+        heightSegments: 16,
+        phiStart: 0,
+        phiLength: twoPi,
+        thetaStart: 0,
+        thetaLength: Math.PI,
+    };
+
+    useEffect(() => {
+        let timer;
+        let _ct = currentTime;
+        if (playing) {
+            timer = setInterval(() => {
+                setwidthSegments(
+                    widthSegments +
+                        (playingTracks[ 
+                            parseInt(
+                                playingTracks.length * (_ct / duration),
+                                10
+                            )
+                        ] / 10000 || 0)
+                );
+                _ct += 1 / (playingTracks.length / duration);
+            }, 1000 / (playingTracks.length / duration));
+        }
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [playingTracks, playing, duration]);
+
     const groupRef = useRef(null);
 
     const lineGeometry = useMemo(() => {
         return new WireframeGeometry(
             new SphereGeometry(
                 data.radius,
-                data.widthSegments,
+                widthSegments,
                 data.heightSegments,
                 data.phiStart,
                 data.phiLength,
@@ -51,25 +67,23 @@ const MyGroup = (props) => {
                 data.thetaLength
             )
         );
-    }, []);
+    }, [widthSegments]);
 
     const meshGeometry = useMemo(() => {
         return new SphereGeometry(
             data.radius,
-            data.widthSegments,
+            widthSegments,
             data.heightSegments,
             data.phiStart,
             data.phiLength,
             data.thetaStart,
             data.thetaLength
         );
-    }, []);
+    }, [widthSegments]);
 
     useFrame(() => {
-        if (playing) {
-            groupRef.current.rotation.x += 0.005;
-            groupRef.current.rotation.y += 0.005;
-        }
+        groupRef.current.rotation.x += 0.005;
+        groupRef.current.rotation.y += 0.005;
     });
 
     return (
@@ -99,7 +113,7 @@ const MyGroup = (props) => {
     );
 };
 
-const ThreeBg = ({ playing }) => {
+const ThreeBg = (props) => {
     return (
         <div id="canvas-container">
             <Canvas
@@ -121,7 +135,7 @@ const ThreeBg = ({ playing }) => {
                     position={new Vector3(-100, -200, -100)}
                 />
                 <Detect />
-                <MyGroup playing={playing} />
+                <MyGroup {...props} />
             </Canvas>
         </div>
     );
